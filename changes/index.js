84899f07a8505e95e647b2fbdc5be1b86a714d09
Sajjad Anwar <me@sajjad.in>
2017-02-01 17:36:33 +0530

    4 spaces

--- changes/index.js
+++ changes/index.js
@@ -8,28 +8,28 @@ var db = require('./db');
 module.exports = processFile;
 
 function processFile(options, callback) {
-  options = options || {};
-  options.isInitial = options.initial || false;
-  options.filename = options.filename || 'data/objects-latest.osc';
-  options.pgURL = options.pgURL || process.env.OSM_COMMENTS_POSTGRES_URL || 'postgres://postgres@localhost/osm-comments';
-  pg.connect(options.pgURL, function(err, client, done) {
-    if (err) {
-      return console.error('could not connect to postgres', err);
-    }
-    parseObjects(options, client, function() {
-      done();
-      if (callback) callback();
+    options = options || {};
+    options.isInitial = options.initial || false;
+    options.filename = options.filename || 'data/objects-latest.osc';
+    options.pgURL = options.pgURL || process.env.OSM_COMMENTS_POSTGRES_URL || 'postgres://postgres@localhost/osm-comments';
+    pg.connect(options.pgURL, function(err, client, done) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        parseObjects(options, client, function() {
+            done();
+            if (callback) callback();
+        });
     });
-  });
 }
 
 function parseObjects(options, client, callback) {
-  counter(options, function(err, changes) {
-    if (err) {
-      console.log('change file parsing failed', err);
-      return callback(err);
-    }
+    counter(options, function(err, changes) {
+        if (err) {
+            console.log('change file parsing failed', err);
+            return callback(err);
+        }
 
-    db.saveChanges(client, changes, callback);
-  });
+        db.saveChanges(client, changes, callback);
+    });
 }
