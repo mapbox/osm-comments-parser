0cc98fb0301b961d1f17793f2a62feba69e3dac6
Rub21 <ruben@mapbox.com>
2017-01-31 23:38:27 -0500

    indentation 2

--- objects/index.js
+++ objects/index.js
@@ -8,26 +8,26 @@ var counter = require('./counter');
 module.exports = processFile;
 
 function processFile(options, callback) {
-    options = options || {};
-    options.isInitial = options.initial || false;
-    options.filename = options.filename || 'data/objects-latest.osc';
-    options.pgURL = options.pgURL || process.env.OSM_COMMENTS_POSTGRES_URL || 'postgres://postgres@localhost/osm-comments';
-    pg.connect(options.pgURL, function(err, client, done) {
-        if (err) {
-            return console.error('could not connect to postgres', err);
-        }
-        parseObjetcs(options, client, function() {
-            done();
-            if (callback) callback();
-        });
+  options = options || {};
+  options.isInitial = options.initial || false;
+  options.filename = options.filename || 'data/objects-latest.osc';
+  options.pgURL = options.pgURL || process.env.OSM_COMMENTS_POSTGRES_URL || 'postgres://postgres@localhost/osm-comments';
+  pg.connect(options.pgURL, function(err, client, done) {
+    if (err) {
+      return console.error('could not connect to postgres', err);
+    }
+    parseObjetcs(options, client, function() {
+      done();
+      if (callback) callback();
     });
+  });
 }
 
 function parseObjetcs(options, client, callback) {
-    decompres(options, function(err, opts) {
-        if (err) return console.log('error decompresing the file ' + err);
-        counter(opts, function(users) {
-            console.log(users);
-        });
+  decompres(options, function(err, opts) {
+    if (err) return console.log('error decompresing the file ' + err);
+    counter(opts, function(users) {
+      console.log(users);
     });
+  });
 }
