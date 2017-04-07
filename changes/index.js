42a4b90ea94d37a81b1d4544e5e971487cce2b85
Kushan Joshi <0o3ko0@gmail.com>
2017-04-07 16:55:35 +0530

    Fix duplicate stats data

--- changes/index.js
+++ changes/index.js
@@ -7,12 +7,27 @@ var db = require('./db');
 
 module.exports = processFile;
 
+// 'statefile' => 2121010
+function extractReplicationId(path) {
+    var stateFile = fs.readFileSync(path, 'utf8');
+    var replicationId = stateFile.split('\n')[2];
+    replicationId = replicationId.split('=')[1];
+    replicationId = parseInt(replicationId, 10);
+    if (Number.isNaN(replicationId)) {
+        throw new Error('replication id is not a number');
+    }
+    return replicationId;
+}
 function processFile(options, callback) {
+    if (!options.statefile) {
+        throw new Error('state file was not provided');
+    }
     options = options || {};
     options.isInitial = options.initial || false;
     options.filename = options.filename || 'data/objects-latest.osc';
-    options.statefile = options.statefile || false;
+    options.statefile = options.statefile;
     options.pgURL = options.pgURL || process.env.OSM_COMMENTS_POSTGRES_URL || 'postgres://postgres@localhost/osm-comments';
+    options.replicationId = extractReplicationId(options.statefile);
     pg.connect(options.pgURL, function(err, client, done) {
         if (err) {
             return console.error('could not connect to postgres', err);
---
+++ tester.js
@@ -0,0 +1,21 @@
+
+var parser = require('./index');
+var mdt = require('mapbox-data-team').getEverything();
+
+var fs = require('fs');
+
+var filepath = __dirname + '/test/fixtures/' + '670-shiv.osc.gz';
+var statepath =  __dirname + '/test/fixtures/' + '480-shiv.state.txt';
+parser.parseChanges({
+    'filename': filepath,
+    'statefile': statepath,
+    'users': mdt
+}, function (err2) {
+    if (err2) {
+        process.stdout.write('minutely processing failed', err2);
+        return process.exit(1);
+    }
+    process.stdout.write('done processing minutely changes, deleting tmp file');
+    process.exit(0);
+    // fs.unlinkSync(filepath);
+});
